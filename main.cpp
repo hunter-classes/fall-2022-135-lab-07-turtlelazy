baf9dcf382bab9549de8a39cf0b57a4621dd9999
turtlelazy <ishraqmahid@gmail.com>
2022-10-20 12:17:01 -0400

    main file tests on unformatted input as directed

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 #include "funcs.h"
-
+#include <fstream>
+#include <sstream>
 
 int main(){
+    std::ifstream file("bad.cpp");
+    std::string str;
+    if (file)
+    {
+        std::ostringstream ss;
+        ss << file.rdbuf(); // reading data
+        str = ss.str();
+    }
+    //std::cout << str;
 
-    std::string test_string = "                int main(){\n"
-       " // Hi, I'm a program!\n"
-        "int x = 1;\n"
-    "for (int i = 0; i < 10; i++){\n"
-     "   cout << i;\n"
-     "   cout << endl;\n"
-    "}\n"
-"}\n";
-
-    std::cout << removeLeadingSpaces(test_string) << "\n";
-
-std::cout << countChar("bob { { { lives in a{ very nice{ house with nice furiniture", '{') << "\n";
-std::cout << pretiffy(test_string) << "\n";
-
-return 0;
+    std::cout << pretiffy(str) << "\n";
+    return 0;
 }
